e50a167c22008113bd78f00611de1edd67f7710c
ycyogesh <ycyogesh183@gmail.com>
2022-08-17 22:39:01 +0530

    print elements using function

---
+++ print_elements_fun.py
@@ -0,0 +1,12 @@
+# Write a function that takes an array & print every elements in an array a
+
+a = [6,8,3,2,1]
+def func(b):
+    for i in range(0,len(b)):
+        print(b[i],end=" ")
+
+func(a)
+
+
+
+
