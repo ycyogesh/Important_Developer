7d5e6b0c4ba85825878c561b12b5f2f991d90d1f
ycyogesh <ycyogesh183@gmail.com>
2022-08-18 16:44:01 +0530

    min element using function

--- min_element_fun.py
+++ min_element_fun.py
@@ -1,14 +1,13 @@
 # Write a function that takes an array & return min element in it
 
 a = [6,8,3,2,1]
-min = 0
+
 
 def fmin(b):
+    min = b[0]
     for i in range(0,len(b)):
-        min = a[0]
-        for j in range(1,len(b)):
-            if(min>a[j]):
-                min = a[j]
+            if(min>a[i]):
+                min = a[i]
     return min
 
 print(fmin(a))
