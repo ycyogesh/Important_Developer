7bc7ffa732f0ac9b35625b96c5878ecc5129f7e3
ycyogesh <ycyogesh183@gmail.com>
2022-08-18 16:44:59 +0530

    min element

--- print_min_element.py
+++ print_min_element.py
@@ -5,7 +5,6 @@ a = [6,8,3,2,1]
 min = 0
 for i in range(0,len(a)):
     min = a[0]
-    for j in range(1,len(a)):
-        if(a[j]<min):
-            min = a[j]
+    if(a[i]<min):
+        min = a[i]
 print(min)
