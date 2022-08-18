d49b70e2bda4a36186ac54e2fbe49abf3ca773a6
ycyogesh <ycyogesh183@gmail.com>
2022-08-18 16:40:01 +0530

    print element max

--- print_max_element.py
+++ print_max_element.py
@@ -2,10 +2,8 @@
 
 a = [6,8,3,2,1]
 
-max = 0
+max = a[0]
 for i in range(0,len(a)):
-    max = a[0]
-    for j in range(1,len(a)):
-        if(a[j]>max):
-            max = a[j]
+    if(a[i]>max):
+        max = a[i]
 print(max)
