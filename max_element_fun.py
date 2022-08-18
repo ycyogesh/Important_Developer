144f0febfd3ebd88af41695c771d63640c882e91
ycyogesh <ycyogesh183@gmail.com>
2022-08-18 16:42:51 +0530

    max element using function

--- max_element_fun.py
+++ max_element_fun.py
@@ -1,14 +1,13 @@
 # Write a function that takes an array & return max element in it
 
 a = [6,8,3,2,1]
-max = 0
+
 
 def fmax(b):
+    max = a[0]
     for i in range(0,len(b)):
-        max = a[0]
-        for j in range(1,len(b)):
-            if(max<a[j]):
-                max = a[j]
+        if(max<a[i]):
+            max = a[i]
     return max
 
 print(fmax(a))
